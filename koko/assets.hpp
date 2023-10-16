c3e62dff383f6285c8049ee212fa9f7edf2891fc
koko <baguskokolearn@gmail.com>
2023-10-16 22:27:47 +0700

    Add Render for Score Window

--- koko/assets.hpp
+++ koko/assets.hpp
@@ -1,54 +1,95 @@
 #include <unistd.h>
+#include <string.h>
 #include "../ncurses/ncurses.h"
 
-class Screen {
+class Snake {
 	private: 
-		int Height;
-		int Width;
-		int VerticalPosition;
-		int HorizontalPosition;
-		const char* NameScreen;
-		//int LengthNameScreen;
+		// Member Property to  Windows Size
+		struct Size {
+			int Height;
+			int Width;
+		};
+
+		// Member Property to Windows Coordinat
+		struct Coordinat {
+			int Vertical;
+			int Horizontal;
+		};
+
+		// Member Property to Windows Name
+		const char* NameTitle;
+		const char* ScoreTitle;
+		
+		// Member Property to Create Windows
+		struct Window {
+			WINDOW *Map;
+			WINDOW *Score;
+		};
+
+		// Member Property to Get Length Windows Name
+		struct Length {
+			size_t Name;
+			size_t Score;
+		};
+
+		// Member Property to Windows Name Position
+		struct Position {
+			int Name;
+			int Score;
+
+			Coordinat PositionCoordinat;
+		};
 
 	public:
-		Screen(int height, int width, int verticalPosition, int horizontalPosition, const char* nameScreen) {
-			Screen::Height = height;
-			Screen::Width = width;
-			Screen::VerticalPosition = verticalPosition;
-			Screen::HorizontalPosition = horizontalPosition;
-			Screen::NameScreen = nameScreen;
-			//Screen::LengthNameScreen = nameScreen.size();
+		// Define Struct
+		Length TitleLength;
+		Position Start;
+		Window Screen;
+		Size Window;                                                                                                                                                                                    
+		
+		Snake(int height, int width, int verticalPosition, int horizontalPosition, const char* gameTitle, const char* scoreTitle) {
+			Window.Height = height;
+			Window.Width = width;
+
+			Start.PositionCoordinat.Vertical = verticalPosition;
+			Start.PositionCoordinat.Horizontal = horizontalPosition;
+
+			Snake::NameTitle = gameTitle;
+			Snake::ScoreTitle = scoreTitle;
+
+			// Map Window Inisialization
+			Screen.Map = newwin(Window.Height, Window.Width, Start.PositionCoordinat.Vertical, Start.PositionCoordinat.Horizontal);
+
+			// Score Window Inisialization
+			Screen.Score = newwin(Window.Height / 4, Window.Width / 3, Start.PositionCoordinat.Vertical + 7.5, Start.PositionCoordinat.Horizontal + Window.Width + 5); 
+			// Ex : (20 / 4, 50 / 3, 0 + 7.5, 0 + 50 + 5)
+			// 20 = Height
+			// 50 = Width
+			// 0 = Position Vertical
+			// 0 Position Horizontal
+			// 50 Position Horizontal after Width and + 5 for space
+
+			TitleLength.Name = strlen(gameTitle);
+			TitleLength.Score = strlen(scoreTitle);
+
+			Start.Name = (Window.Width - TitleLength.Name) / 2;
+			Start.Score = ((Window.Width / 3) - TitleLength.Score) / 2; // Ex : ((50 / 3) - 9) / 2)
 		}
 
 		void render() {
-			initscr();
-			WINDOW *window = newwin(Height, Width, VerticalPosition, HorizontalPosition);
-		    box(window, 0, 0);
-		    curs_set(FALSE);
-    
-		    while(true) {
-				mvwprintw(window, 0, 19, NameScreen);
-				wrefresh(window);
-
-		    //    for(int i = 2; i < 49; i++) {
-			//		mvwprintw(window, 2, i - 1, "++++0");
-			//		//mvwprintw(MainWindow, 2, i, "++++0");
-			//		//mvwprintw(ScoreWindow, 2.5, 9.5, "0"); 
-			//		wrefresh(window);
-			//		//wrefresh(ScoreWindow);
-			//		sleep(1);
-			//	}
+		    box(Screen.Map, 0, 0);
+		    box(Screen.Score, 0, 0);
+		    curs_set(FALSE); 
+			while(true) {
+				mvwprintw(Screen.Map, 0, Start.Name, NameTitle);
+				mvwprintw(Screen.Score, 0, Start.Score, ScoreTitle);
+				wrefresh(Screen.Map);
+				wrefresh(Screen.Score);
+				sleep(1);
 			}
+		}
+
+		~Snake(){
 			endwin();
 		}
 };
-
-
-class Position {
-	public:
-		 
-};
-
-
-
-
--- main.cpp
+++ main.cpp
@@ -2,13 +2,15 @@
 #include "koko/assets.hpp"
 
 // To do : 
-// Render for Score Window
 // Create Random Position for Food in map object
 
 int main() {
-	Screen Map(20, 50, 0, 0, std::string("[ Snake CLI ]").c_str());
-   
-	Map.render();
+	initscr();
+	const char gameTitle[] = "[ Snake CLI ]";
+	const char scoreTitle[] = "[ Score ]";
+	Snake Map(20, 50, 0, 0, gameTitle, scoreTitle);
 
+	Map.render();
+	
 	return 0;
 }
